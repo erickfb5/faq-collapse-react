7541ee9f8767f603741ccb891a4bdb729571e843
Erick Brandao <brandaoerick@MacBook-Air.local>
2023-03-07 14:46:57 -0300

    first version

---
+++ src/App.js
@@ -0,0 +1,39 @@
+import { useState } from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faChevronDown, faTimes } from "@fortawesome/free-solid-svg-icons";
+
+import { faqData } from "./faqData";
+import "./App.css";
+
+const App = () => {
+  const [activeIndex, setActiveIndex] = useState(0);
+
+  const handleToggle = (index) =>
+    setActiveIndex(activeIndex === index ? -1 : index);
+
+  return (
+    <div className="App">
+      <h1>Frequently Asked Questions</h1>
+      <div className="faq-container">
+        {faqData.map((faq, index) => (
+          <div
+            key={index}
+            className={`faq ${index === activeIndex ? "active" : ""}`}
+          >
+            <h3 className="faq-title">{faq.title}</h3>
+            <p className="faq-text">{faq.text}</p>
+            <button className="faq-toggle" onClick={() => handleToggle(index)}>
+              {index === activeIndex ? (
+                <FontAwesomeIcon icon={faTimes} />
+              ) : (
+                <FontAwesomeIcon icon={faChevronDown} />
+              )}
+            </button>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/faqData.js
@@ -0,0 +1,22 @@
+export const faqData = [
+    {
+      title: "Why shouldn't we trust atoms?",
+      text: "They make up everything",
+    },
+    {
+      title: "What do you call someone with no body and no nose?",
+      text: "Nobody knows.",
+    },
+    {
+      title: "What's the object-oriented way to become wealthy?",
+      text: "Inheritance.",
+    },
+    {
+      title: "How many tickles does it take to tickle an octopus?",
+      text: "Ten-tickles!",
+    },
+    {
+      title: "What is: 1 + 1?",
+      text: "Depends on who are you asking.",
+    },
+  ];
